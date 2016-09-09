3bb95c4dea601e9e38dade07ff73577661f08a57
Jess <jessicakopps@gmail.com>
2016-09-09 16:38:51 +0000

    inicilization of workspace jessica

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');//teste git
 
 class Blog extends CI_Controller 
 {
