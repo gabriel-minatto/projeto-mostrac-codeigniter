6c67f93fc5beaba28aaf96b308ab3b586777f0c0
Jess <jessicakopps@gmail.com>
2016-09-12 16:30:50 +0000

    ajustando base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://code-igniter-mostratec-gabdevilshunter.c9users.io/';
+$config['base_url'] = 'https://projeto-mostratec-codeigniter-jessicakopps.c9users.io/';
 
 /*
 |--------------------------------------------------------------------------
